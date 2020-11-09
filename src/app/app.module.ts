cdaf0e5e7699239b2ae2636ce9b6721305bf6249
Cipriano <natancipriano98@gmail.com>
2020-11-08 21:22:02 -0300

    Update new project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './controller/user/user.component';
+import { AgencyComponent } from './controller/agency/agency.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    AgencyComponent
   ],
   imports: [
     BrowserModule,
